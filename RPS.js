ad68a394bb63eb2ec5224ee1c64638084b6823d0
Xavierize <xkajack@gmail.com>
2023-07-20 14:36:55 -0400

    Updated Title--

--- RPS.js
+++ RPS.js
@@ -1,1 +1,9 @@
 console.log("Hello World");
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice("Rock","Paper","Scissors");
+  console.log(playRound(playerSelection, computerSelection));
