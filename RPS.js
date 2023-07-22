5864c67d165f8191f67545db609aa8e67c3da7d5
Xavierize <xkajack@gmail.com>
2023-07-22 17:22:16 -0400

    Planning to add more components to the code.

--- RPS.js
+++ RPS.js
@@ -1,32 +1,43 @@
-console.log("Hello World");
-// Setting up functionality for the game
-window.computerScore = 0;
-window.tie = 0;
-let playerSelection = "";
-let result = "";
-
-function playRound(playerSelection, computerSelection) {
-
-    let player = playerSelection.toLowerCase();
-    let status = '';
-    let winner = '';
-    let loser = '';
-    
-    //If the player chooses rock 
-    if (player === 'rock' && computerSelection === 'paper') {
-        status = 'Lose :(';
-        winner = 'Paper';
-        loser = 'Rock';
-        ComputerScore++;
-
-        function selectRock(e) {
-            playerSelection = 'rock';
-            //console.log('Rock!')
-            game();
+    function playRound(playerSelection, computerSelection) {
+    // Rock Paper Scissors game logic
+    if (playerSelection === computerSelection) {
+      return "It's a tie! :/";
+    } else if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+      return "You win! :D";
+    } else {
+      return "You lose! :(";
     }
+  }
 
-    function selectRock(e) {
-        playerSelection = 'rock';
-        //console.log('Rock!')
-        game();
-}
+  function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+  }
+
+  function selectRock() {
+    const playerSelection = "rock";
+    const computerSelection = getComputerChoice();
+    showResult(playRound(playerSelection, computerSelection));
+  }
+
+  function selectPaper() {
+    const playerSelection = "paper";
+    const computerSelection = getComputerChoice();
+    showResult(playRound(playerSelection, computerSelection));
+  }
+
+  function selectScissors() {
+    const playerSelection = "scissors";
+    const computerSelection = getComputerChoice();
+    showResult(playRound(playerSelection, computerSelection));
+  }
+
+  function showResult(result) {
+    const winnerDiv = document.querySelector(".winner");
+    winnerDiv.textContent = result;
+  }
