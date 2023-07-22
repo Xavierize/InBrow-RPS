b4b2521ea45b9b2c67c924a29f0679fd1eaaed64
Xavierize <xkajack@gmail.com>
2023-07-22 17:21:09 -0400

    Updated! and fixed the functionality error!

--- RPS.js
+++ RPS.js
@@ -1,9 +1,32 @@
 console.log("Hello World");
+// Setting up functionality for the game
+window.computerScore = 0;
+window.tie = 0;
+let playerSelection = "";
+let result = "";
 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
-  }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice("Rock","Paper","Scissors");
-  console.log(playRound(playerSelection, computerSelection));
+
+    let player = playerSelection.toLowerCase();
+    let status = '';
+    let winner = '';
+    let loser = '';
+    
+    //If the player chooses rock 
+    if (player === 'rock' && computerSelection === 'paper') {
+        status = 'Lose :(';
+        winner = 'Paper';
+        loser = 'Rock';
+        ComputerScore++;
+
+        function selectRock(e) {
+            playerSelection = 'rock';
+            //console.log('Rock!')
+            game();
+    }
+
+    function selectRock(e) {
+        playerSelection = 'rock';
+        //console.log('Rock!')
+        game();
+}
